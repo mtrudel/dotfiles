ea6116d48aab99505968892cc72fea41705d88d4
Mat Trudel <mtrudel@pagerduty.com>
2023-01-10 13:29:40 -0500

    Tighten up LSP config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,9 +1,5 @@
 local lspconfig = require("lspconfig")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-local on_attach = function(_, bufnr)
-  require("cmp_nvim_lsp").default_capabilities(capabilities)
-end
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 --- Set up Elixir LSP
 local path_to_elixirls = vim.fn.expand("~/Code/elixir-ls/release/language_server.sh")
@@ -11,7 +7,6 @@ local path_to_elixirls = vim.fn.expand("~/Code/elixir-ls/release/language_server
 lspconfig.elixirls.setup({
   cmd = {path_to_elixirls},
   capabilities = capabilities,
-  on_attach = on_attach,
   settings = {
     elixirLS = {
       dialyzerEnabled = false,
@@ -21,6 +16,5 @@ lspconfig.elixirls.setup({
 })
 
 lspconfig.eslint.setup({
-  on_attach = on_attach,
   capabilities = capabilities,
 })
