f9f8bc6951a0c86203393b46915c5bfd6ade786b
Mat Trudel <mtrudel@pagerduty.com>
2023-01-10 13:28:36 -0500

    Minor package tweaks

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -12,9 +12,11 @@ startup({
   function(use, use_rocks)
     use{"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
     use("neovim/nvim-lspconfig")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
     use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("hrsh7th/nvim-cmp")
     use("hrsh7th/cmp-vsnip")
     use("hrsh7th/vim-vsnip")
     use("overcache/NeoSolarized")
