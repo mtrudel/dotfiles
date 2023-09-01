c6c2847653eeefcd34b736a00eef40a1451069b7
Mat Trudel <mat@geeky.net>
2023-08-31 22:15:19 -0400

    Install ripgrep as part of nvim plugin

--- nvim/install.zsh
+++ nvim/install.zsh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 
-# Install nvim if it's not already installed
+# Install nvim if it's not already installed. Do this as a separate step in case the user has nvim
+# installed separate from brew
 if ! command -v nvim &> /dev/null; then
   brew install -q neovim
 fi
+
+brew install -q ripgrep
 
 # Copy over zshrc.d contents
 ln -fns ${0:A:h}/zshrc.d/* ~/.zshrc.d/
