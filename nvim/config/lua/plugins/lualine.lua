44391925a1d34dd00ba537e572d35ec7e9bf3823
Mat Trudel <mat@geeky.net>
2023-09-04 17:17:09 -0400

    Tweak lualine

--- nvim/config/lua/plugins/lualine.lua
+++ nvim/config/lua/plugins/lualine.lua
@@ -1,4 +1,18 @@
 return { {
   'nvim-lualine/lualine.nvim',
-  opts = {},
+  opts = {
+    options = {
+      component_separators = '|',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff' },
+      lualine_c = { { 'filename', path = 3 } },
+      lualine_x = { 'diagnostics', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+    extensions = { 'nvim-tree' },
+  },
 } }
