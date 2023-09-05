9407c5da62bfabda58626e53c4fe91b5a2a73a59
Mat Trudel <mat@geeky.net>
2023-09-05 10:03:23 -0400

    Add history search based on current line content

--- zsh/zshrc.d/history.zsh
+++ zsh/zshrc.d/history.zsh
@@ -1,11 +1,19 @@
 HISTSIZE=5000
 HISTFILE=~/.zsh_history
 SAVEHIST=5000
-HISTDUP=erase
 setopt appendhistory
+setopt extendedhistory
 setopt sharehistory
 setopt incappendhistory
 setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_ignore_dups
 setopt hist_find_no_dups
+
+# Up and down arrows search history based on currently typed line
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
