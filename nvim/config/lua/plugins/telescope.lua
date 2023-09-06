0a666b051c11159ee6d49f8dc6f6801e3f9ff291
Mat Trudel <mat@geeky.net>
2023-09-06 09:19:56 -0400

    Use live_grep in telescope

--- nvim/config/lua/plugins/telescope.lua
+++ nvim/config/lua/plugins/telescope.lua
@@ -14,6 +14,6 @@ return { {
     { '<leader>b', function() require('telescope.builtin').oldfiles({ include_current_session = true, previewer = false }) end },
     { '<leader>T', function() require('telescope.builtin').git_files({ previewer = false }) end },
     { '<leader>t', function() require('telescope.builtin').find_files({ previewer = false }) end },
-    { '<leader>f', function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') }) end },
+    { '<leader>f', function() require('telescope.builtin').live_grep() end },
   },
 } }
