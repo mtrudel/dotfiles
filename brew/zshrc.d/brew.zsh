d29a8c4054e495630da12cefc0b8d53ba60030f4
Mat Trudel <mat@geeky.net>
2023-09-10 21:56:33 -0400

    Use brew-provided zsh completions

--- brew/zshrc.d/brew.zsh
+++ brew/zshrc.d/brew.zsh
@@ -2,3 +2,5 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 [[ -f /opt/homebrew/bin/brew ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
 [[ -f /usr/local/bin/brew ]] && eval "$(/usr/local/bin/brew shellenv)"
+
+FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
