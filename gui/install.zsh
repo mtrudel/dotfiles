8f61948e2d18af58e25a0fbc15483d62cab1e3a0
Mat Trudel <mat@geeky.net>
2023-09-27 15:25:17 -0400

    Minor GUI installer enhancements

--- gui/install.zsh
+++ gui/install.zsh
@@ -16,19 +16,24 @@ brew install -q switchaudio-osx
 
 brew install -q --cask 1password
 brew install -q --cask alfred
-brew install -q --cask docker
 brew install -q --cask deckset
+brew install -q --cask displaperture
+brew install -q --cask docker
 brew install -q --cask gitup
 brew install -q --cask google-chrome
 brew install -q --cask iterm2
 brew install -q --cask kicad
+brew install -q --cask mist
 brew install -q --cask qcad
 brew install -q --cask rectangle
 brew install -q --cask slack
 brew install -q --cask spotify
+brew install -q --cask superduper
 brew install -q --cask tableplus
 brew install -q --cask visual-studio-code
+brew install -q --cask zoom
 
+mas install 1569813296 # 1Password Safari Extension
 mas install 1018301773 # Adblock Pro
 mas install 1544743900 # Hush
 mas install 409203825 # Numbers
@@ -41,7 +46,7 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
- 
+
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
@@ -49,7 +54,7 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
 defaults write com.apple.CrashReporter DialogType -string "none"
- 
+
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
@@ -64,3 +69,5 @@ defaults write com.apple.finder DisableAllAnimations -bool true
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+defaults write com.knollsoft.Rectangle almostMaximizeWidth -float 0.6
+defaults write com.knollsoft.Rectangle almostMaximizeHeight -float 1.0
