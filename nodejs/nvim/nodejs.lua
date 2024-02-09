29afa8953ec375611b20efadb509e5e37cd421db
Mat Trudel <mtrudel@pagerduty.com>
2024-02-09 12:28:22 -0500

    Prefer eslint to tsserver

--- nodejs/nvim/nodejs.lua
+++ nodejs/nvim/nodejs.lua
@@ -1,4 +1,10 @@
-require('lspconfig').tsserver.setup({
+require('lspconfig').eslint.setup({
   capabilities = require('cmp_nvim_lsp').default_capabilities(),
-  on_attach = require('mtrudel/helpers/lsp').on_attach,
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = true
+    client.server_capabilities.documentRangeFormattingProvider = true
+
+    require('mtrudel/helpers/lsp').on_attach(client, bufnr)
+  end
+
 })
