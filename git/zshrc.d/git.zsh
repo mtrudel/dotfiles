4cb65810c1ddcbecb2abc0267d63f8b068c402a3
Mat Trudel <mtrudel@pagerduty.com>
2024-03-13 10:44:02 -0400

    Tighten up git aliases

--- git/zshrc.d/git.zsh
+++ git/zshrc.d/git.zsh
@@ -1,5 +1,2 @@
 function gu() { gitup commit }
 function hb() { hub browse }
-function pr() { git push origin -u && hub pull-request $* }
-function gpo() { git push origin -u }
-function gpof() { git push origin -uf }
