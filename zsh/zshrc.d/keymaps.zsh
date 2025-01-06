08391f112d707e4166105c9858cdda4f6079c24a
Mat Trudel <mat@geeky.net>
2025-01-06 16:41:15 -0500

    Tighten up keymapping to work out of the box on iTerm & Zed

--- zsh/zshrc.d/keymaps.zsh
+++ zsh/zshrc.d/keymaps.zsh
@@ -1,2 +1,10 @@
-bindkey ";9D" backward-word
-bindkey ";9C" forward-word
+# Option + arrows in most embedded terminals
+bindkey "^[b" backward-word
+bindkey "^[f" forward-word
+
+# Option + arrows in OOTB iTerm
+bindkey "^[[1;3D" backward-word
+bindkey "^[[1;3C" forward-word
+
+# Option + delete
+bindkey  "^["  backward-delete-word
