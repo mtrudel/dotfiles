cc397f0923fe645a607f1d4eed2b0322fb8802f7
Mat Trudel <mat@geeky.net>
2026-07-04 21:30:58 -0400

    Update git messages to proerly escape commands

--- git/install.zsh
+++ git/install.zsh
@@ -15,9 +15,9 @@ ln -fns ${0:A:h}/gitconfig ~/.gitconfig
 ln -fns ${0:A:h}/gitignore ~/.gitignore
 
 if ! git config --get user.name > /dev/null; then
-  echo "No git username set! Add one via 'git config -f ~/.gitconfig.private --add user.name <Moe Fonebone>'"
+  echo "No git username set! Add one via 'git config -f ~/.gitconfig.private --add user.name \"<Moe Fonebone>\"'"
 fi
 
 if ! git config --get user.email > /dev/null; then
-  echo "No git email set! Add one via 'git config -f ~/.gitconfig.private --add user.email <moe@fonebone.com>'"
+  echo "No git email set! Add one via 'git config -f ~/.gitconfig.private --add user.email \"<moe@fonebone.com>\"'"
 fi
