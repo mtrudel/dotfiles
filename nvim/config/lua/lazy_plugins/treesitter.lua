905df9429a90846ff9dc5a9db313783e85a55b9e
Mat Trudel <mat@geeky.net>
2026-07-09 17:42:50 -0400

    Update to treesitter to pull from proper branch

--- nvim/config/lua/lazy_plugins/treesitter.lua
+++ nvim/config/lua/lazy_plugins/treesitter.lua
@@ -1,8 +1,11 @@
 return { {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  build = ':TSUpdate',
   main = 'nvim-treesitter',
   opts = {
     auto_install = true,
+    ensure_installed = { 'c', 'query', 'vim', 'vimdoc' },
     highlight = { enable = true },
     indent = { enable = true },
   },
